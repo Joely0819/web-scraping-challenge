31e169ed6f548f42ff4fcfd3b92ce3e461dfabee
Joely Alemar <joelyalemar@gmail.com>
2022-01-08 06:26:28 -0500

    Updates

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[52]:
-
 
 # Dependencies
 from bs4 import BeautifulSoup
@@ -12,151 +7,100 @@ import pandas as pd
 import pymongo
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager 
-
-
-# In[53]:
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 
 
 # Setup splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
-
-
+def scrape_all():
+    mars_data={}
 # NASA News
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-# In[54]:
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
 
+    news_title = soup.find_all('div', class_='content_title')[0].text
+    news_title
 
-# URL of page to be scraped
-url = 'https://redplanetscience.com/'
-
-## Retrieve page
-browser.visit(url)
-
-
-# In[55]:
-
-
-# Create BeautifulSoup object; parse with 'html.parser'
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[56]:
-
-
-#Find Latest News Title
-# Examine the results and look for a div with the class 'content_title'
-news_title = soup.find_all('div', class_='content_title')[0].text
-news_title
-
-
-# In[57]:
-
-
-#Find Latest news Paragraph text
-# Examine the results and look for a div with the class 'article_teaser_body'
-news_p = soup.find_all('div', class_='article_teaser_body')[0].text
-news_p
+    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+    news_p
 
 
 # JPL image
-
-# In[58]:
-
-
 #find the image url for the current Featured Mars Image 
-jpl_url = "https://spaceimages-mars.com/"
-browser.visit(jpl_url)
+    jpl_url = "https://spaceimages-mars.com/"
+    browser.visit(jpl_url)
 
-mars_img = browser.html
-soup = BeautifulSoup(mars_img, 'html.parser')
+    mars_img = browser.html
+    soup = BeautifulSoup(mars_img, 'html.parser')
 
-jpl_src = soup.find('img', class_='headerimage fade-in')['src']
+    jpl_src = soup.find('img', class_='headerimage fade-in')['src']
  
 #assign the url string to a variable called featured_image_url
-featured_image_url = jpl_url+jpl_src
-featured_image_url
+    featured_image_url = jpl_url+jpl_src
+    featured_image_url
 
 
 # Mars Facts
 
-# In[59]:
-
-
-#get url
-url = 'https://galaxyfacts-mars.com/'
-#Use the read_html function in Pandas to automatically scrape any tabular data from a page.
-tables = pd.read_html(url)
-tables
-
-
-# In[60]:
-
-
-type(tables)
-
-
-# In[61]:
-
-
+    url = 'https://galaxyfacts-mars.com/'
+    tables = pd.read_html(url)
+    tables
+    type(tables)
 #Create dataframe
-df = tables[0]
-df.head()
-
-
-# In[63]:
-
-
+    df = tables[0]
+    df.head()
 #generate html table
-html_table = df.to_html()
-html_table
+    html_table = df.to_html(classes="table table-striped")
+    html_table
 
 
 # Mars Hemispheres
 
-# In[64]:
-
-
 #get url
-mars_url = 'https://marshemispheres.com/'
-browser.visit(mars_url)
-html=browser.html
-soup=BeautifulSoup(html,'html.parser')
+    mars_url = 'https://marshemispheres.com/'
+    browser.visit(mars_url)
+    html=browser.html
+    soup=BeautifulSoup(html,'html.parser')
 
 
-# In[65]:
 
 
 # get hemisphere elements
-mars_hemi = soup.find ('div',class_= 'collapsible results')
-mars_item=mars_hemi.find_all('div',class_='item')
+    mars_hemi = soup.find ('div',class_= 'collapsible results')
+    mars_item=mars_hemi.find_all('div',class_='item')
 
-
-# In[66]:
 
 
 # create empty list and loop through items
-hemi_image_urls=[]
+    hemi_image_urls=[]
 #loop 
-for item in mars_item:
-    try:
-        hemi=item.find('div',class_='description')
-        title=hemi.h3.text
-        img_urls=hemi.a['href']
-        browser.visit(mars_url+img_urls)
-        html=browser.html
-        soup=BeautifulSoup(html,'html.parser')
-        image_src=soup.find('li').a['href']
-        if (title and image_src):
-            print(title)
-            print(image_src)
-    except Exception as e:
-        print(e)
+    for item in mars_item:
+        try:
+            hemi=item.find('div',class_='description')
+            title=hemi.h3.text
+            img_urls=hemi.a['href']
+            browser.visit(mars_url+img_urls)
+            html=browser.html
+            soup=BeautifulSoup(html,'html.parser')
+            image_src=soup.find('li').a['href']
+            if (title and image_src):
+                print(title)
+                print(image_src)
+        except Exception as e:
+            print(e)
 
-
-# In[67]:
-
+    # create mars_data dict that we can insert into mongo
+    mars_data = {
+        "news_title": news_title,
+        "news_paragraph": news_p,
+        "featured_image": featured_image_url,
+        "facts": html_table,
+        "hemispheres": hemi_image_urls
+        }
 
 browser.quit()
-
